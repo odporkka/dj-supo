975853e04a98d99af5f3dab76922bca836ec5911
Otto Porkka <otto.porkka@iki.fi>
2022-05-14 17:54:53 +0300

    Fix some issues with recording

--- src/components/views/HomePage.jsx
+++ src/components/views/HomePage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { listen } from "../../services/micListener";
 import {
   askNotificationPermission,
-  postNotification
 } from "../../services/notificationService";
 import {
   getNewWordList,
--- src/services/micListener.js
+++ src/services/micListener.js
@@ -4,6 +4,7 @@ const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogni
 const iceCreamTruck1 = new SpeechRecognition();
 iceCreamTruck1.continuous = true;
 iceCreamTruck1.lang = 'fi-FI';
+iceCreamTruck1.interimResults = true;
 
 const listen = ( updateWordList ) => {
     iceCreamTruck1.start();
@@ -11,6 +12,15 @@ const listen = ( updateWordList ) => {
         const length = event.results.length;
         const result = event.results[length - 1][0].transcript;
         updateWordList(result);
+    }
+    iceCreamTruck1.onend = function(event) {
+        console.log('Supo man fell asleep, waking up..');
+        iceCreamTruck1.start();
+        iceCreamTruck1.onresult = function(event) {
+            const length = event.results.length;
+            const result = event.results[length - 1][0].transcript;
+            updateWordList(result);
+        }
     }
 }
 
