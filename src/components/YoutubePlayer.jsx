fea97847202ee2ca32a356399201b96dd733265b
Joonas <joonas.mertanen@gmail.com>
2022-05-14 16:04:54 +0300

    show active word

--- src/components/YoutubePlayer.jsx
+++ src/components/YoutubePlayer.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function({ query }) {
+export default function ({ query }) {
   const [youtubeId, setYoutubeId] = useState()
   useEffect(() => {
     (async () => {
@@ -13,12 +13,25 @@ export default function({ query }) {
     })()
   }, [query])
 
-  return youtubeId && (
-    <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style={{
-      position: "absolute",
-      left: 0,
-      top: 0,
-      zIndex: -1
-    }}></iframe>
+  return (
+    <>
+      {youtubeId && (
+        <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            left: 0,
+            top: 0,
+            zIndex: -1
+          }}></iframe>
+      )}
+      <div style={{
+        position: "absolute",
+        left: 30,
+        bottom: 60,
+        backgroundColor: "white",
+        padding: 4
+      }}>{query}</div>
+    </>
   )
 }
