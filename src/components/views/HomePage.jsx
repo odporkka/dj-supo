6fdc50eede3bb1eb7420c9d0720a4c53bedea712
Jere Vaara <jerevaara@gmail.com>
2022-05-14 16:17:56 +0300

    ribul rabl

--- src/components/views/HomePage.jsx
+++ src/components/views/HomePage.jsx
@@ -1,28 +1,39 @@
-import React, {useEffect, useState} from 'react';
-import { listen } from '../../services/micListener';
-import { getNewWordList, getMostUsedWord } from '../../services/transcriptService';
+import React, { useEffect, useState } from "react";
+import { listen } from "../../services/micListener";
+import {
+  askNotificationPermission,
+  postNotification,
+} from "../../services/notificationService";
+import {
+  getNewWordList,
+  getMostUsedWord,
+} from "../../services/transcriptService";
 import YoutubePlayer from "../YoutubePlayer";
 
 const HomePage = (props) => {
-    const [ wordList, setWordList ] = useState(undefined)
-    useEffect(() => {
-        listen(updateWordList);
-    }, []);
-    const updateWordList = (newWords) => {
-        console.log(newWords);
-        setWordList((wordList) => getNewWordList(wordList, newWords));
-    }
-    console.log(wordList);
+  const [wordList, setWordList] = useState(undefined);
+  useEffect(() => {
+    askNotificationPermission();
+    listen(updateWordList);
+    setTimeout(() => {
+      postNotification("Nice music taste mate");
+    }, 1000);
+  }, []);
+  const updateWordList = (newWords) => {
+    console.log(newWords);
+    setWordList((wordList) => getNewWordList(wordList, newWords));
+  };
+  console.log(wordList);
 
-    return (
-        <div id="homePage">
-            <p>DJ SUPO</p>
-            <img src="/djlogo.png" alt="logo" />
-            { wordList &&
-                <YoutubePlayer query={getMostUsedWord(wordList, setWordList)} />
-            }
-        </div>
-    )
-}
+  return (
+    <div id="homePage">
+      <p>DJ SUPO</p>
+      <img src="/djlogo.png" alt="logo" />
+      {wordList && (
+        <YoutubePlayer query={getMostUsedWord(wordList, setWordList)} />
+      )}
+    </div>
+  );
+};
 
 export default HomePage;
